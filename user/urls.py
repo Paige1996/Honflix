c1362d86369f39565102e728a6cc01ddf2a81010
Parkjeonghunn <gkrwkd95@gmail.com>
2022-01-29 22:38:12 +0900

    add.user views.py

---
+++ user/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('sign-up/', views.sign_up_view, name='sign-up'), #회원가입 URL 설정한 것
+    path('sign-in/', views.sign_in_view, name='sign-in'), #로그인 URL 설정한 것
+]
--- user/views.py
+++ user/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
+def sign_up_view(request):
+    if request.method == 'GET': #GET 메서드로 요청이 들어온 경우
+        return render(request, 'user/signup.html')
+    elif request.method == 'POST': #POST 메서드로 요청이 들어온 경우
+        return ""
+def sign_in_view(request):
+    return render(request, 'user/signin.html')
+
