b2970b232efd3940bbf24c0cadd29bdbdf163d8a
Parkjeonghunn <gkrwkd95@gmail.com>
2022-01-31 22:23:34 +0900

    update user

--- user/views.py
+++ user/views.py
@@ -1,13 +1,19 @@
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect
 from .models import UserModel
 from django.http import HttpResponse
 from django.contrib.auth import get_user_model #사용자가 있는지 검수하는 함수
 from django.contrib import auth # 사용자 auth 가능
 
+
 # Create your views here.
 def sign_up_view(request):
     if request.method == 'GET': #GET 메서드로 요청이 들어온 경우
-        return render(request, 'user/signup.html')
+        user = request.user.is_authenticated # 로그인 된 사용자가 요청하는지 검사
+        if user: # 로그인이 되어있다면
+            return redirect('/')
+        else:
+            return render(request, 'user/signup.html')
     elif request.method == 'POST': #POST 메서드로 요청이 들어온 경우
         username = request.POST.get('username', None)
         password = request.POST.get('password', None)
@@ -22,11 +28,15 @@ def sign_up_view(request):
                 return render(request, 'user/signup.html') #사용자가 존재하기 때문에, 사용자를 저장하지 않고 회원가입 페이지 다시 띄움
             else:
                 UserModel.objects.create_user(username=username, password=password, nickname=nickname, email=email)
-                return redirect('/sign-in')
+                return redirect('/sign-in') #회원가입이 완료되었으므로 로그인 페이지로 이동
 
 def sign_in_view(request):
     if request.method == 'GET':
-        return render(request, 'user/signin.html')
+        user = request.user.is_authenticated #사용자가 로그인 되어 있는지 검사
+        if user: #로그인이 되어 있다면
+            return redirect('/')
+        else:
+            return render(request, 'user/signin.html')
     elif request.method == 'POST':
         username = request.POST.get('username', None)
         password = request.POST.get('password', None)
@@ -34,10 +44,14 @@ def sign_in_view(request):
         me = auth.authenticate(request, username=username, password=password) #암호화된 비밀번호와 현재 입력된 비밀번호가 일치하는지, 그게 사용자와 맞는지까지 한번에 확인
         if me is not None: #사용자가 있다
             auth.login(request, me) #장고가 알아서 로그인도 관리, 나의 정보를 넣어줌
-            return HttpResponse("로그인 성공!")
+            return redirect('/')
         else:
-            return redirect('/sign-in')
+            return redirect('/sign-in') #로그인 실패
 
+@login_required
+def logout(request):
+    auth.logout(request) #인증되어있는 정보를 없애기
+    return redirect("/")
 
 #signup.html에 추가해야할 것들
 #회원가입 form 태그 찾아서 거기에
